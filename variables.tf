f65cc2eb4e1936e364dcdc64482c52503780ef97
Myint <luvmyo@gmail.com>
2021-12-29 19:22:24 +0700

    apigw,iam,lambda added

--- variables.tf
+++ variables.tf
@@ -1,7 +1,31 @@
+variable "environment" {
+    description = "Env"
+    default     = "dev"
+}
+
+variable "name" {
+    description = "Application Name"
+    type        = string
+}
+
+locals {
+    description = "Aplication Name"
+    app_name = "${var.name}-${var.environment}"
+}
+
+variable "region" {
+    default = "us-east-1"
+}
+
+variable "lambda_name" {
+    description = "Name for lambda function"
+    default = "lambda"
+}
 variable "name" {
   description = "This is the human-readable name of the queue. If omitted, Terraform will assign a random name."
   type        = string
 }
+
 
 variable "message_retention_seconds" {
   description = "The number of seconds Amazon SQS retains a message. Integer representing seconds, from 60 (1 minute) to 1209600 (14 days)"
